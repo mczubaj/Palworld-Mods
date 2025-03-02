074ae2571acb827298fa3391e0f271ff761e4cca
Michał Czubaj <czubaij@gmail.com>
2025-03-02 19:57:55 +0100

    add custom flat value handling

--- LyleenPercentageHeal/Scripts/config.lua
+++ LyleenPercentageHeal/Scripts/config.lua
@@ -3,7 +3,7 @@ local config = {
   HEAL_PERCENTAGE_PER_RANK = { 20, 40, 55, 70, 85 },
 
   -- Flat value. How much hp will be healed if % heal would end up healing less than this value
-  -- 0 = default
+  -- 0 means original game value
   HEAL_FLAT_VALUE_PER_RANK = { 0, 0, 0, 0, 0 }
 }
 
--- LyleenPercentageHeal/Scripts/main.lua
+++ LyleenPercentageHeal/Scripts/main.lua
@@ -6,26 +6,25 @@ local isHooked = false;
 
 local function handlePercentageHeals(Context)
   if isHooked then return end
+  isHooked = true
 
-  local palUtil = StaticFindObject("/Script/Pal.Default__PalUtility")
-  local playerStats = palUtil:GetPlayerCharacter(Context:get()):GetCharacterParameterComponent()
-  local otomoHolder = palUtil:GetOtomoHolderComponent(Context:get())
+  local palUtility = StaticFindObject("/Script/Pal.Default__PalUtility")
+  local playerStats = palUtility:GetPlayerCharacter(Context:get()):GetCharacterParameterComponent()
+  local otomoHolder = palUtility:GetOtomoHolderComponent(Context:get())
 
   RegisterHook("/Script/Pal.PalPartnerSkillParameterComponent:GetActiveSkillMainValueByRank", function(Context)
     local skillParams = Context:get()
-
     if skillParams:GetSkillName() ~= LYLEEN_PARTNER_SKILL_NAME then return end
 
     local spawnedPalRank = otomoHolder:TryGetSpawnedOtomo():GetCharacterParameterComponent():GetIndividualParameter()
         :GetRank()
     local percentageHealAmount = (playerStats:GetMaxHP().Value / 1000) *
         (config.HEAL_PERCENTAGE_PER_RANK[spawnedPalRank] / 100)
-    -- add handling custom flat values
-    local bestHealAmount = math.max(percentageHealAmount, skillParams.ActiveSkill_MainValueByRank[spawnedPalRank])
+    local customFlatHealAmount = config.HEAL_FLAT_VALUE_PER_RANK[spawnedPalRank]
+    local highestHealAmount = math.max(percentageHealAmount,
+      (customFlatHealAmount ~= 0 and customFlatHealAmount) or skillParams.ActiveSkill_MainValueByRank[spawnedPalRank])
 
-    isHooked = true
-
-    return bestHealAmount
+    return highestHealAmount
   end)
 end
 
