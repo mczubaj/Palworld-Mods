1b8d1cb9c0e00d9ac22c337b7a05f24439c70368
Michał Czubaj <czubaij@gmail.com>
2025-03-05 14:47:57 +0100

    Lyleen percentage heals (#1)
    * init
    
    * cleanup, add ServerAcknowledgePossession hooking
    
    * added config file
    
    * add custom flat value handling
    
    * rename mod folder
    
    * rename mod folder, omit PalSchema in gitignore

---
+++ LyleenPercentageHeal/Scripts/config.lua
@@ -0,0 +1,10 @@
+local config = {
+  -- How much % of player's max hp will be healed for each pal rank (AKA stars)
+  HEAL_PERCENTAGE_PER_RANK = { 20, 40, 55, 70, 85 },
+
+  -- Flat value. How much hp will be healed if % heal would end up healing less than this value
+  -- 0 means original game value
+  HEAL_FLAT_VALUE_PER_RANK = { 0, 0, 0, 0, 0 }
+}
+
+return config
