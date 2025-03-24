f01bf10222773d0e52b7b91c72e907b466b489c3
Michał Czubaj <czubaij@gmail.com>
2025-03-24 14:25:46 +0100

    Partner heal overhaul (#5)
    * added Teafant, Petallia, added configurable cooldown
    
    * removed mod folder from gitignore

---
+++ HealingPartnerSkillsOverhaul/Scripts/config.lua
@@ -0,0 +1,55 @@
+local config = {
+
+  -- config for Lyleen
+  lyleen = {
+    -- How much % of player's max hp will be healed for each pal rank (AKA stars)
+    healPercentagePerRank = { 50, 55, 65, 80, 100 },
+
+    -- Flat value. How much hp will be healed if % heal would end up healing less than this value
+    -- 0 means original game value
+    healFlatValuePerRank = { 0, 0, 0, 0, 0 },
+
+    -- Cooldown in seconds. 0 means original game value
+    cooldown = 0,
+
+    -- Don't touch these unless you know what you're doing
+    skillName = "Heal_LilyQueen",
+    isCooldownHooked = false
+  },
+
+  -- config for Petallia
+  petallia = {
+    -- How much % of player's max hp will be healed for each pal rank (AKA stars)
+    healPercentagePerRank = { 20, 22, 26, 32, 40 },
+
+    -- Flat value. How much hp will be healed if % heal would end up healing less than this value
+    -- 0 means original game value
+    healFlatValuePerRank = { 0, 0, 0, 0, 0 },
+
+    -- Cooldown in seconds. 0 means original game value
+    cooldown = 0,
+
+    -- Don't these this unless you know what you're doing
+    skillName = "Heal_FlowerDoll",
+    isCooldownHooked = false
+  },
+
+  -- config for Teafant
+  teafant = {
+    -- How much % of player's max hp will be healed for each pal rank (AKA stars)
+    healPercentagePerRank = { 10, 11, 13, 16, 20 },
+
+    -- Flat value. How much hp will be healed if % heal would end up healing less than this value
+    -- 0 means original game value
+    healFlatValuePerRank = { 0, 0, 0, 0, 0 },
+
+    -- Cooldown in seconds. 0 means original game value
+    cooldown = 0,
+
+    -- Don't touch these unless you know what you're doing
+    skillName = "Heal",
+    isCooldownHooked = false
+  }
+}
+
+return config
--- HealingPartnerSkillsOverhaul/Scripts/main.lua
+++ HealingPartnerSkillsOverhaul/Scripts/main.lua
@@ -1,7 +1,5 @@
 local config = require "config"
 
-local LYLEEN_PARTNER_SKILL_NAME = FName("Heal_LilyQueen")
-
 local isHooked = false;
 
 local function handlePercentageHeals(Context)
@@ -13,16 +11,31 @@ local function handlePercentageHeals(Context)
   local otomoHolder = palUtility:GetOtomoHolderComponent(Context:get())
 
   RegisterHook("/Script/Pal.PalPartnerSkillParameterComponent:GetActiveSkillMainValueByRank", function(Context)
-    local skillParams = Context:get()
-    if skillParams:GetSkillName() ~= LYLEEN_PARTNER_SKILL_NAME then return end
+    local invokedSkillParams = Context:get()
+    local skillConfig = nil
+
+    for _, v in pairs(config) do
+      if v.skillName == invokedSkillParams:GetSkillName():ToString() then
+        skillConfig = v
+      end
+    end
+
+    if skillConfig == nil then return end
+
+    if not skillConfig.isCooldownHooked and skillConfig.cooldown ~= 0 then
+      invokedSkillParams.coolDownTimeMax.value = skillConfig.cooldown * 1000
+      invokedSkillParams.CoolDownTime.value = skillConfig.cooldown * 1000
+      skillConfig.isCooldownHooked = true
+    end
 
     local spawnedPalRank = otomoHolder:TryGetSpawnedOtomo():GetCharacterParameterComponent():GetIndividualParameter()
         :GetRank()
     local percentageHealAmount = (playerStats:GetMaxHP().Value / 1000) *
-        (config.HEAL_PERCENTAGE_PER_RANK[spawnedPalRank] / 100)
-    local customFlatHealAmount = config.HEAL_FLAT_VALUE_PER_RANK[spawnedPalRank]
+        (skillConfig.healPercentagePerRank[spawnedPalRank] / 100)
+    local customFlatHealAmount = skillConfig.healFlatValuePerRank[spawnedPalRank]
     local highestHealAmount = math.max(percentageHealAmount,
-      (customFlatHealAmount ~= 0 and customFlatHealAmount) or skillParams.ActiveSkill_MainValueByRank[spawnedPalRank])
+      (customFlatHealAmount ~= 0 and customFlatHealAmount) or
+      invokedSkillParams.ActiveSkill_MainValueByRank[spawnedPalRank])
 
     return highestHealAmount
   end)
