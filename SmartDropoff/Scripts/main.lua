bf24ecfae655dc5930ce8c7954e27bb82951bd25
Michał Czubaj <czubaij@gmail.com>
2025-03-26 18:36:26 +0100

    refactor pt2 - move smart logic to separate func, rename top scope vars

--- SmartDropoff/Scripts/main.lua
+++ SmartDropoff/Scripts/main.lua
@@ -8,15 +8,15 @@ local hotkeysEnabled = false
 local palUtility
 
 ---@type UPalMapObjectConcreteModelBase
-local container
+local targetStorageContainer
 ---@type APlayerController
 local player
 ---@type UPalCommonScrollListBase
-local widget
+local playerInventoryWidget
 
 -- Returns items from player's inventory and opened storage container
 local function GetItems()
-  local containerItems = container:GetItemContainerModule():GetContainer().ItemSlotArray
+  local containerItems = targetStorageContainer:GetItemContainerModule():GetContainer().ItemSlotArray
   local inventoryData = palUtility:GetPlayerState(player):GetInventoryData()
   -- magic numbers from EPalItemTypeA enum, importing the actual enum is not possible AFAIK
   local itemTypes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }
@@ -52,53 +52,79 @@ local function GetMatches()
   return matched
 end
 
-local function StoreItems(playerInventorySlots, storageContainerId, widget)
+local function StoreItems(playerInventorySlots, storageContainerId, playerInventoryWidget)
   for index = 1, #playerInventorySlots do
-    widget:MoveItem(1, playerInventorySlots[index], storageContainerId)
+    playerInventoryWidget:MoveItem(1, playerInventorySlots[index], storageContainerId)
   end
 end
 
-local function Cleanup()
-  ---@diagnostic disable: cast-local-type
-  container = nil
-  widget = nil
-  ---@diagnostic enable: cast-local-type
+local function StoreSmart()
+  local matchedItems = GetMatches()
 
-  hotkeysEnabled = false
-end
+  local lookup = {}
+  for index = 1, #matchedItems do
+    local staticId = matchedItems[index]:get().ItemId.StaticId:ToString()
+    lookup[staticId] = true
+  end
 
-local function StoreAll()
-  local storageContainerId = container:GetItemContainerModule():GetContainerId()
+  local storageContainerId = targetStorageContainer:GetItemContainerModule():GetContainerId()
+  ---@type TArray<UPalItemContainer>
   local playerInvContainers = palUtility:GetPlayerState(player):GetInventoryData().InventoryMultiHelper.Containers
   -- //TODO: magic number
   -- maybe solve with UPalItemContainerMultiHelper:FindByStaticItemIds - use current inv contents to find container
   local playerInvContainer = playerInvContainers[1]
 
-  StoreItems(playerInvContainer.ItemSlotArray, storageContainerId, widget)
+  for index = 1, #playerInvContainer.ItemSlotArray do
+    local slot = playerInvContainer.ItemSlotArray[index]
+    local staticId = slot.ItemId.StaticId:ToString()
+
+    if lookup[staticId] then
+      playerInventoryWidget:MoveItem(1, slot, storageContainerId)
+    end
+  end
+end
+
+local function StoreAll()
+  local storageContainerId = targetStorageContainer:GetItemContainerModule():GetContainerId()
+  local playerInvContainers = palUtility:GetPlayerState(player):GetInventoryData().InventoryMultiHelper.Containers
+  -- //TODO: magic number
+  -- maybe solve with UPalItemContainerMultiHelper:FindByStaticItemIds - use current inv contents to find container
+  local playerInvContainer = playerInvContainers[1]
+
+  StoreItems(playerInvContainer.ItemSlotArray, storageContainerId, playerInventoryWidget)
+end
+
+local function Cleanup()
+  ---@diagnostic disable: cast-local-type
+  targetStorageContainer = nil
+  playerInventoryWidget = nil
+  ---@diagnostic enable: cast-local-type
+
+  hotkeysEnabled = false
 end
 
 RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(PlayerController)
   if isHooked then return end
   isHooked = true
 
+  ---@type APlayerController
+  player = PlayerController:get().Pawn
   ---@type UPalUtility
   ---@diagnostic disable-next-line: assign-type-mismatch
   palUtility = StaticFindObject("/Script/Pal.Default__PalUtility")
-  ---@type APlayerController
-  player = PlayerController:get().Pawn
 
   RegisterHook("/Script/Pal.PalMapObjectConcreteModelBase:OnTriggerInteract",
-    function(Container, _, InteractionType)
+    function(TargetStorageContainer, _, InteractionType)
       print("Container interact triggered")
 
-      container = Container:get()
+      targetStorageContainer = TargetStorageContainer:get()
 
       RegisterHook(
         "/Game/Pal/Blueprint/UI/Inventory/WBP_PalPlayerInventoryScrollList.WBP_PalPlayerInventoryScrollList_C:Construct",
-        function(Widget)
+        function(PlayerInventoryWidget)
           print("Widget construct triggered")
 
-          widget = Widget:get()
+          playerInventoryWidget = PlayerInventoryWidget:get()
           hotkeysEnabled = true
         end)
 
@@ -121,29 +147,7 @@ RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(PlayerCon
     print("Smart dropoff hotkey pressed")
     palUtility:SendSystemAnnounce(player, "Smart dropoff hotkey pressed!")
 
-    local matchedItems = GetMatches()
-
-    local lookup = {}
-    for index = 1, #matchedItems do
-      local staticId = matchedItems[index]:get().ItemId.StaticId:ToString()
-      lookup[staticId] = true
-    end
-
-    local storageContainerId = container:GetItemContainerModule():GetContainerId()
-    ---@type TArray<UPalItemContainer>
-    local playerInvContainers = palUtility:GetPlayerState(player):GetInventoryData().InventoryMultiHelper.Containers
-    -- //TODO: magic number
-    -- maybe solve with UPalItemContainerMultiHelper:FindByStaticItemIds - use current inv contents to find container
-    local playerInvContainer = playerInvContainers[1]
-
-    for index = 1, #playerInvContainer.ItemSlotArray do
-      local slot = playerInvContainer.ItemSlotArray[index]
-      local staticId = slot.ItemId.StaticId:ToString()
-
-      if lookup[staticId] then
-        widget:MoveItem(1, slot, storageContainerId)
-      end
-    end
+    StoreSmart()
   end)
 
   RegisterKeyBind(config.ALL_DROPOFF_HOTKEY, function()
